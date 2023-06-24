6fbba011137ec95b92adee699cb66d8581c26bf7
Cristian <cprado416@gmail.com>
2023-06-23 19:42:36 -0700

    Create playRound function.

--- script.js
+++ script.js
@@ -7,4 +7,25 @@ function getComputerChoice() {
   } else return "Scissors";
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  playerSelection =
+    playerSelection.slice(0, 1).toUpperCase() +
+    playerSelection.slice(1).toLowerCase();
+  if (playerSelection === computerSelection) {
+    return `Tie! You both chose ${playerSelection}.`;
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    return "You lose! Paper beats Rock";
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    return "You lose! Scissors beat Paper";
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You lose! Rock beats Scissors";
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You win! Rock beats Scissors";
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You win! Paper beats Rock";
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You win! Scissors beat Paper";
+  }
+}
+
+

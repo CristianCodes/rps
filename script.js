32721ae2f5929ce0425abdf3248c3ae4375fce54
Cristian <cprado416@gmail.com>
2023-07-07 19:28:38 -0700

    removed unused game function, added DOM methods, changed layout of html page

--- script.js
+++ script.js
@@ -8,57 +8,40 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = getComputerChoice;
-  if (playerSelection === computerSelection) {
-    console.log(`Tie! You both chose ${playerSelection}`);
-    return 0;
-  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-    console.log("You lose! Paper beats Rock");
-    return 1;
-  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-    console.log("You lose! Scissors beat Paper");
-    return 1;
-  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-    console.log("You lose! Rock beats Scissors");
-    return 1;
-  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-    console.log("You win! Rock beats Scissors");
-    return 2;
-  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-    console.log("You win! Paper beats Rock");
-    return 2;
-  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-    console.log("You win! Scissors beat Paper");
-    return 2;
-  }
+  if (Number(rounds.textContent) < 5) {
+    if (playerSelection === computerSelection) {
+      winner.textContent = `TIE! you both chose ${playerSelection}`;
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+      cScore.textContent = `${Number(cScore.textContent) + 1}`;
+      winner.textContent = `Round Lost - Computer chose ${computerSelection}`;
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+      cScore.textContent = `${Number(cScore.textContent) + 1}`;
+      winner.textContent = `Round Lost - Computer chose ${computerSelection}`;
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+      cScore.textContent = `${Number(cScore.textContent) + 1}`;
+      winner.textContent = `Round Lost - Computer chose ${computerSelection}`;
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+      pScore.textContent = `${Number(pScore.textContent) + 1}`;
+      winner.textContent = `Round Won - Computer chose ${computerSelection}`;
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+      pScore.textContent = `${Number(pScore.textContent) + 1}`;
+      winner.textContent = `Round Won - Computer chose ${computerSelection}`;
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+      pScore.textContent = `${Number(pScore.textContent) + 1}`;
+      winner.textContent = `Round Won - Computer chose ${computerSelection}`;
+    }
+    rounds.textContent = `${Number(rounds.textContent) + 1}`;
+  } else stateWinner();
 }
 
-function game() {
-  let comp = 0;
-  let player = 0;
-
-  let playerChoice = prompt("Type in Rock Paper or Scissors");
-  let result = playRound(playerChoice, getComputerChoice());
-  console.log(result);
-  if (result === 1) {
-    comp++;
-  } else if (result === 2) {
-    player++;
-  }
-  console.log(`Player Score:${player} Computer Score:${comp}`);
-
-  // if (player > comp) {
-  //   console.log(
-  //     `You beat the Computer -Final Score- Player Score:${player} Computer Score ${comp}`
-  //   );
-  // } else if (comp > player) {
-  //   console.log(
-  //     `The Computer beat You -Final Score- Player Score:${player} Computer Score ${comp}`
-  //   );
-  // } else
-  //   console.log(
-  //     `The game ends in a Tie -Final Score- Player Score:${player} Computer Score ${comp} `
-  //   );
+function stateWinner(playerChoice) {
+  let player = Number(pScore.textContent);
+  let comp = Number(cScore.textContent);
+  if (player > comp) {
+    winner.textContent = "YOU BEAT THE COMPUTER!";
+  } else if (comp > player) {
+    winner.textContent = "THE COMPUTER BEAT YOU!";
+  } else winner.textContent = "GAME ENDS IN A TIE";
 }
 
 // Button Selectors
@@ -72,6 +55,14 @@ const pScore = document.querySelector("#pScore");
 const cScore = document.querySelector("#cScore");
 const winner = document.querySelector("#winner");
 
-//Img Holder Selectors
-const compImg = document.querySelector("#comp");
-const playerImg = document.querySelector("#player");
+rock.addEventListener("click", () => {
+  playRound("Rock", getComputerChoice());
+});
+
+paper.addEventListener("click", () => {
+  playRound("Paper", getComputerChoice());
+});
+
+scissors.addEventListener("click", () => {
+  playRound("Scissors", getComputerChoice());
+});
